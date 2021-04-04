f13e017c744486fa3760eff30fee81987700057f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-04 22:47:25 +0300

    unnecessary comments deleted

--- src/App.js
+++ src/App.js
@@ -8,32 +8,6 @@ import EnteryPage from './Pages/EntryPage.js';
 
 const App = () => {
   useCSS('../src/ToDoApp.css');
-  const desc = `Orginize it all with TodoList. Join millions of people who organize
-    work and life with Todoist`;
-
-  //   const loginFunc = () => {
-  //     const loginPage = createElement('div', null, null, 'Login Page');
-  //     const target = document.querySelector('.container');
-  //     target.innerHTML = '';
-  //     target.appendChild(loginPage);
-  //   };
-
-  //   const signUpFunc = () => {
-  //     const signUpPage = createElement('div', null, null, 'SignUp Page');
-  //     const target = document.querySelector('.container');
-  //     target.innerHTML = '';
-  //     target.appendChild(signUpPage);
-  //   };
-
-  //   const entryPage = () => {
-  //     const target = document.querySelector('.container');
-
-  //     if (!target.firstElementChild.classList.contains('login-page')) {
-  //       const entery = EnteryPage('TO DO', desc, { loginFunc, signUpFunc });
-  //       target.innerHTML = '';
-  //       target.appendChild(entery);
-  //     }
-  //   };
 
   const entery = EnteryPage(TITLE, DESCRIPTION, {
     loginFunc: loginPageLink,
